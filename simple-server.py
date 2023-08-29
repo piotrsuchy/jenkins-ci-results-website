8e86edd5e3d64540c2375522e717b9f071b7ea60
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-08-30 01:02:08 +0200

    DB tables scopes tests and setups integrated

--- simple-server.py
+++ simple-server.py
@@ -1,6 +1,6 @@
-import os
-import psycopg2
-from flask import Flask, request, render_template
+import os, psycopg2, json
+from psycopg2 import sql
+from flask import Flask, request, render_template, jsonify
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,6 +14,30 @@ app.config['DB_CONN'] = psycopg2.connect(
     port=os.environ.get("DB_PORT")
 )
 
+conn = app.config['DB_CONN']
+
+# filling setups table with records from config json
+def populate_setups_table():
+    with open('setups_config.json') as f:
+        config = json.load(f)
+
+    conn = app.config['DB_CONN']
+    cur = conn.cursor()
+
+    # Check if table is empty
+    cur.execute("SELECT COUNT(*) FROM setups;")
+    count = cur.fetchone()[0]
+    if count > 0:
+        print("Setups table already populated, skipping initialization.")
+        cur.close()
+        return
+
+    for pipeline in config['pipelines']:
+        insert_query = sql.SQL("INSERT INTO setups (name, comment) VALUES (%s, %s);")
+        cur.execute(insert_query, (pipeline['setup'], pipeline.get('comment', '')))
+
+    conn.commit()
+    cur.close()
 
 test_results = []
 
@@ -35,5 +59,67 @@ def post_message():
     })
     return "Message received", 200
 
+@app.route('/post_test_results', methods=['POST'])
+def post_test_results():
+    data = request.get_json()
+
+    conn = app.config['DB_CONN']
+    cur = conn.cursor()
+
+    insert_query = sql.SQL("INSERT INTO tests (name, duration) VALUES (%s, %s) RETURNING test_id;")
+    cur.execute(insert_query, (data['test_name'], data['duration']))
+    test_id = cur.fetchone()[0]
+
+    conn.commit()
+    cur.close()
+
+    return {"message": "Test results written to database", "test_id": test_id}, 200
+
+@app.route('/post_scope_results', methods=['POST'])
+def post_scope_results():
+    data = request.get_json()
+    response = {}
+
+    try:
+        conn = app.config['DB_CONN']
+        cur = conn.cursor()
+
+        # Your updated SQL query and operations here
+        insert_query = """INSERT INTO scopes (setup_id, name, duration)
+                          VALUES (%s, %s, %s) RETURNING scope_id;"""
+
+        # Then use this default setup_id in the SQL query
+        cur.execute(insert_query, (data['setup_id'], data['name'], data['duration']))
+
+        conn.commit()
+        scope_id = cur.fetchone()[0]
+
+        response['message'] = 'Scope results successfully written to database.'
+        response['scope_id'] = scope_id
+        response['status'] = 'success'
+        response['code'] = 200
+
+        cur.close()
+
+    except Exception as e:
+        conn.rollback()
+        print(f"Database Error: {e}")
+
+        response['message'] = 'An error occurred while writing to the database.'
+        response['status'] = 'error'
+        response['code'] = 500
+
+    return jsonify(response), response['code']
+
 if __name__ == '__main__':
+    # for development purposes atm:
+    conn = app.config['DB_CONN']
+    cur = conn.cursor()
+    cur.execute("TRUNCATE setups, scopes, tests, jenkinsinfo, failingtestcases RESTART IDENTITY;")  # Replace 'table_name' with your actual table name
+    conn.commit()
+    cur.close()
+
+    populate_setups_table()
     app.run(port=5000, debug=True)
+
+    
--- tests/PythonListener.py
+++ tests/PythonListener.py
@@ -1,5 +1,29 @@
-import requests
-import sys
+import requests, os
+import json, socket
+import psycopg2  
+from dotenv import load_dotenv
+
+
+def get_local_ip():
+    try:
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+    except Exception:
+        local_ip = '127.0.0.1'
+    return local_ip
+
+def find_setup_by_ip(ip_address, setups):
+    for setup in setups:
+        if setup['ip'] == ip_address:
+            return setup
+    return None
+
+def get_setup_id_by_name(setup_name, conn):
+    cur = conn.cursor()
+    cur.execute("SELECT setup_id FROM setups WHERE name = %s;", (setup_name,))
+    result = cur.fetchone()
+    return result[0] if result else None
+
 
 
 class PythonListener:
@@ -11,20 +35,41 @@ class PythonListener:
     def log_message(self, message):
         # Suppress all log messages
         pass
-
-
-    def start_test(self, name, attributes):
-        pass
     
     def end_test(self, data, result):
-        timestamp = result.endtime
-        suite_name = data.parent.name
-        message1 = f"Suite Name: {suite_name}, Test Name: {data.name}, Status: {result.status}, Timestamp: {timestamp}"
-        self.completed_tests += 1
-        message2 = ''
-        if self.completed_tests % self.interval == 0:
-            message2 = '{self.completed_tests} test cases are done.\n'
-        requests.post("http://127.0.0.1:5000/post_message", data={'message': message1, 'progress': message2})
+        test_data = {
+            'test_name': data.name,
+            'duration': result.elapsedtime / 1000,  # Convert milliseconds to seconds
+        }
+        requests.post("http://127.0.0.1:5000/post_test_results", json=test_data)
 
-    def end_suite(self, name, attributes):
-        sys.stderr.write(f'Total test cases executed: {self.completed_tests}')
+    def end_suite(self, data, result):
+        # Read setups from setups_config.json
+        with open('setups_config.json', 'r') as f:
+            setups = json.load(f)['pipelines']
+
+        local_ip = get_local_ip()
+
+        matching_setup = find_setup_by_ip(local_ip, setups)
+
+        load_dotenv()
+        conn = psycopg2.connect(dbname=os.environ.get("DB_NAME"), 
+                                user=os.environ.get("DB_USER"), 
+                                password=os.environ.get("DB_PASS"), 
+                                host=os.environ.get("DB_HOST"), 
+                                port=os.environ.get("DB_PORT"))
+
+        if matching_setup:
+            setup_name = matching_setup['setup']
+            setup_id = get_setup_id_by_name(setup_name, conn)  # get setup_id from database
+        else:
+            setup_id = "Unknown"
+            setup_name = "Unknown"
+
+        scope_data = {
+            'setup_id': setup_id,
+            'name': setup_name,
+            'duration': result.elapsedtime / 1000,
+        }
+
+        requests.post("http://127.0.0.1:5000/post_scope_results", json=scope_data)
