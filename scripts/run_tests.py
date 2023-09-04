9fe2dbdd3b0be6d52303f3f90bb2116aedf3c228
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-09-04 22:22:18 +0200

    Relative paths added

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,11 +1,30 @@
 import os
+import sys
 import subprocess
 
+
 def run_robot_tests():
+    # Determine the script's directory
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    test_file_path = os.path.join(script_dir, '..', 'tests', 'example_tests.robot')
-    listener_file_path = os.path.join(script_dir, '..', 'tests', 'PythonListener.py')
-    subprocess.run(["robot", "--listener", listener_file_path, test_file_path])
+
+    # Add the project's root directory to the PYTHONPATH
+    project_root = os.path.join(script_dir, "..")
+    if "PYTHONPATH" in os.environ:
+        os.environ["PYTHONPATH"] += os.pathsep + project_root
+    else:
+        os.environ["PYTHONPATH"] = project_root
+
+    # Set the current working directory to the script's directory
+    os.chdir(script_dir)
+
+    # Determine paths for test files and the listener
+    test_file_path = os.path.join(project_root, "tests", "example_tests.robot")
+    listener_file_path = os.path.join(project_root, "tests", "PythonListener.py")
+
+    subprocess.run(
+        ["robot", "--listener", listener_file_path, test_file_path], env=os.environ
+    )
+
 
 if __name__ == "__main__":
     run_robot_tests()
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export PYTHONPATH="$PYTHONPATH:$script_dir/.."
 robot --listener "$script_dir/../tests/PythonListener.py" "$script_dir/../tests/example_tests.robot"
